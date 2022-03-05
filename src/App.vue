00e76f468eda47e5d77393bc8088a2ce310c5bb5
Sumeet Patel <sumeet.patel@eroam.com>
2022-03-05 15:51:57 +0530

    added images and variable colors

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <b-alert variant="success" show>Success Alert</b-alert>
     <router-view />
   </div>
 </template>
