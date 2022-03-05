47d1693fdaa4a95b07506d0d31ac69a79c06676b
Sumeet Patel <sumeet.patel@eroam.com>
2022-03-05 14:50:05 +0530

    updated Variable file and added packages

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//bootstrap-vue
+import { BootstrapVue } from "bootstrap-vue";
+//common SCSS
+import "./assets/scss/app.scss";
+Vue.use(BootstrapVue);
+
+//vue-screen to obtain screen-size
+import VueScreen from "vue-screen";
+Vue.use(VueScreen, "bootstrap");
+
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+Vue.use(VueAwesomeSwiper);
+
 Vue.config.productionTip = false;
 
 new Vue({
